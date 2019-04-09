339a2e9a78cb4662900e51427727c919cf825cf1
Luke Chang <luke.x.chang@gmail.com>
2019-04-09 22:27:10 +1200

    completed middleware part

--- server/server.js
+++ server/server.js
@@ -5,66 +5,78 @@
 // create a middleware function to catch and handle errors, register it
 // as the last middleware on app
 
-
 // create a route middleware for POST /lions that will increment and
 // add an id to the incoming new lion object on req.body
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var _ = require('lodash');
-var morgan = require('morgan');
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var _ = require('lodash')
+var morgan = require('morgan')
 
-var lions = [];
-var id = 0;
+var lions = []
+var id = 0
 
-var updateId = function(req, res, next) {
+var updateId = function (req, res, next) {
   // fill this out. this is the route middleware for the ids
-};
+  id++
+  req.id = id
+  next()
+}
 
 app.use(morgan('dev'))
-app.use(express.static('client'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.static('client'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-
-app.param('id', function(req, res, next, id) {
+app.param('id', function (req, res, next, id) {
   // fill this out to find the lion based off the id
-  // and attach it to req.lion. Rember to call next()
-});
+  // and attach it to req.lion. Remember to call next()
+  const lion = _.find(lions, { id: parseInt(id) })
+  req.lion = lion
+  next()
+})
 
-app.get('/lions', function(req, res){
-  res.json(lions);
-});
+app.get('/lions', function (req, res) {
+  res.json(lions)
+})
 
-app.get('/lions/:id', function(req, res){
+app.get('/lions/:id', function (req, res) {
   // use req.lion
-  res.json(lion || {});
-});
+  const { lion } = req
+  res.json(lion || {})
+})
 
-app.post('/lions', updateId, function(req, res) {
-  var lion = req.body;
+app.post('/lions', updateId, function (req, res) {
+  var lion = req.body
+  lion.id = req.id
+  lions.push(lion)
+  res.status(201)
+  res.json(lion)
+})
 
-  lions.push(lion);
-
-  res.json(lion);
-});
-
-
-app.put('/lions/:id', function(req, res) {
-  var update = req.body;
+app.put('/lions/:id', function (req, res) {
+  var update = req.body
   if (update.id) {
     delete update.id
   }
 
-  var lion = _.findIndex(lions, {id: req.params.id});
+  var lion = _.findIndex(lions, { id: req.params.id })
   if (!lions[lion]) {
-    res.send();
+    res.send()
   } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
+    var updatedLion = _.assign(lions[lion], update)
+    res.json(updatedLion)
   }
-});
+})
 
-app.listen(3000);
-console.log('on port 3000');
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.status(500).send(err)
+  }
+})
+
+const server = app.listen(3000, 'localhost', function() {
+  const { address, port } = server.address()
+  console.log(`listening on http://${address}:${port}`)
+})
